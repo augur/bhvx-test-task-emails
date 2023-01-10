babe09a29d160f4ccbb25bf0e8a3fbbe7eba3966
Denis Kilchichakov <kdenis87@gmail.com>
2023-01-10 11:04:09 +0400

    simple command line app built with maven

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # bhvx-test-task-emails
+
+
+# Build
+```
+mvn clean install
+```
+
+# Run
+```
+mvn exec:java -Dexec.args="input_file.zip" -Dexec.jvmArgs="-Xmx256m"
+```
+Working directory is root of the project
---
+++ src/main/kotlin/com/kilchichakov/emails/parser/Main.kt
@@ -0,0 +1,13 @@
+package com.kilchichakov.emails.parser
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+    val file = File(args.first())
+    println(file.readText())
+}
